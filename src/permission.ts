b3b23362781d51a88b007039a5336cee39034e17
xfy196 <xfy196@outlook.com>
2022-05-04 01:21:41 +0800

    加入播放视频的页面

--- src/permission.ts
+++ src/permission.ts
@@ -1,9 +1,11 @@
 import router from "./router";
 import { useUserStore } from "@/stores/user";
+import { useHostStore } from "@/stores/host";
 
 router.beforeEach(async (to, from, next) => {
   const { checkLogin, isLogin } = useUserStore();
-  if (!isLogin) {
+  const { isInit } = useHostStore();
+  if (!isLogin && isInit) {
     await checkLogin();
     next();
   } else {
